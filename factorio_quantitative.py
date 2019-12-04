2f3d76164e2001dc5d5e5dbba4025d625e7e15ad
waingt <38601498+waingt@users.noreply.github.com>
2019-12-04 20:32:14 +0800

    refactored

--- factorio_quantitative.py
+++ factorio_quantitative.py
@@ -1,13 +1,23 @@
-"""
-Run in python 3
-recipe cache or module pyparsing is required
-(you can install pyparsing by running "pip install pyparsing")
-"""
-FACTORIO_RECIPE_FOLDER = r"D:\Program Files (x86)\Steam\steamapps\common\Factorio\data\base\prototypes\recipe/"
-
 import os, re, json, code, ctypes
 from fractions import Fraction
 from heapq import heapify, heappush, heappop
+from configparser import ConfigParser
+
+from pyparsing import (
+    Suppress,
+    CharsNotIn,
+    Word,
+    Literal,
+    alphanums,
+    nums,
+    Forward,
+    delimitedList,
+    Dict,
+    Group,
+    ZeroOrMore,
+    Optional,
+)
+import yaml
 
 
 def file_cache(func):
@@ -26,6 +36,7 @@ def file_cache(func):
 @file_cache
 def get_recipe():
     recipe = {}
+    recipe_path = r"data\base\prototypes\recipe/"
     recipe_files = [
         "recipe",
         "inserter",
@@ -34,20 +45,6 @@ def get_recipe():
         "demo-furnace-recipe",
         "demo-recipe",
     ]
-    from pyparsing import (
-        Suppress,
-        CharsNotIn,
-        Word,
-        Literal,
-        alphanums,
-        nums,
-        Forward,
-        delimitedList,
-        Dict,
-        Group,
-        ZeroOrMore,
-        Optional,
-    )
 
     def Syntax():
         delimitedList = (
@@ -71,16 +68,13 @@ def get_recipe():
             if type(k) is list:
                 k[1] = int(k[1])
                 result.append(k)
-                # item_dict[k[0]] = None
             else:
                 result.append([k["name"], int(k["amount"])])
-                # item_dict[k['name']] = None
         return result
 
     recipe_parser = Syntax()
-    # item_dict = {}
     for f in recipe_files:
-        s = open(FACTORIO_RECIPE_FOLDER + f + ".lua").read()
+        s = open(FACTORIO_PATH + recipe_path + f + ".lua").read()
         s = re.sub(r"data:extend\(\s*\{\s*\{", "", s, count=1)
         s = re.sub(r"--.*?\n", "", s)
         d = re.split(r"\n  \},\s+  \{", s)
@@ -140,16 +134,14 @@ def get_recipe():
 
 
 def get_single_product_dict():
-    banned_names = set(
-        [
-            "light-oil-cracking",
-            "heavy-oil-cracking",
-            "solid-fuel-from-petroleum-gas",
-            "solid-fuel-from-heavy-oil",
-            "nuclear-fuel-reprocessing",
-            "basic-oil-processing",
-        ]
-    )
+    banned_names = {
+        "light-oil-cracking",
+        "heavy-oil-cracking",
+        "solid-fuel-from-petroleum-gas",
+        "solid-fuel-from-heavy-oil",
+        "nuclear-fuel-reprocessing",
+        "basic-oil-processing",
+    }
     single_product_dict = {}
     for name, [input_, output, time, category] in recipe_dict.items():
         time = Fraction(time)
@@ -203,6 +195,13 @@ def get_complexity_dict():
     return complexity_dict
 
 
+def preprocessing_schemes(config):
+    for s in config["SCHEMES"]:
+        for k, v in config[s].items():
+            f1, f2 = v
+            config[s][k] = [Fraction(f1), Fraction(f2)]
+
+
 def fraction_format(f: Fraction) -> str:
     t = -(-f.numerator * 10 // f.denominator)
     t = str(t)
@@ -236,26 +235,15 @@ STD_OUT_HANDLE = ctypes.windll.kernel32.GetStdHandle(-11)
 
 
 def set_color(color):
+    if color not in COLOR_DICT:
+        print("available colors:")
+        print(*COLOR_DICT.keys(), sep="\n")
+        raise ValueError("wrong color: %s" % color)
     ctypes.windll.kernel32.SetConsoleTextAttribute(STD_OUT_HANDLE, COLOR_DICT[color])
 
 
-SEPARATOR = "-" * 40
-FLUIDS = {
-    "water",
-    "petroleum-gas",
-    "crude-oil",
-    "steam",
-    "light-oil",
-    "heavy-oil",
-    "lubricant",
-    "sulfuric-acid",
-}
-
-
 def record(count: Fraction, name: str, output, input_):
-    s = "{0:>7} {1:<10}: {2} <= {3}".format(
-        fraction_format(count), name, output, input_
-    )
+    s = "{0:>7} {1:<5}: {2} <= {3}".format(fraction_format(count), name, output, input_)
     print(s)
 
 
@@ -264,25 +252,25 @@ def estimate_belts(d: dict):
         if k in FLUIDS:
             b = ""
         elif v <= Fraction(15, 2):
-            b = ": half belt"
+            b = ": " + translater["half belt"]
         elif v <= 15:
-            b = ": half fast belt or 1 belt"
+            b = ": " + translater["half fast belt or 1 belt"]
         elif v <= Fraction(45, 2):
-            b = ": half express belt or 1 fast belt"
+            b = ": " + translater["half express belt or 1 fast belt"]
+        elif v <= 30:
+            b = ": " + translater["1 fast belt"]
         else:
-            b = ": " + str((v / 45).__ceil__()) + " express belt"
-        s = "{0:>7} {1:<10}{2}".format(fraction_format(v), k, b)
+            b = ": " + str((v / 45).__ceil__()) + " " + translater["express belt"]
+        s = "{0:>7} {1:<5}{2}".format(fraction_format(v), translater[k], b)
         print(s)
 
 
 def oil_solve(item: dict, scheme=None, beautify_output=True):
     if scheme is None:
         scheme = normal
-    X_ORDER = ["advanced-oil-processing", "heavy-oil-cracking", "light-oil-cracking"]
-    Y_ORDER = ["petroleum-gas", "light-oil", "heavy-oil"]
     oil_recipe = []
     x = []
-    for t in X_ORDER:
+    for t in OIL_RECIPES:
         input_, output, time, category = recipe_dict[t]
         time = Fraction(time)
         speed, prod = scheme[category]
@@ -291,12 +279,15 @@ def oil_solve(item: dict, scheme=None, beautify_output=True):
         for i in input_:
             result[i[0]] = result.get(i[0], 0) + i[1] / time
         for i in output:
-            result[i[0]] = result.get(i[0], 0) - i[1] / time * prod
-        for k in Y_ORDER:
+            result[i[0]] = result.get(i[0], 0) - i[1] / time
+        for k, v in result.items():
+            if v < 0:
+                result[k] = v * prod
+        for k in OIL_KINDS:
             x.append(result.get(k, 0))
         oil_recipe.append(result)
-    output_ = {k: v for k, v in item.items() if k in Y_ORDER}
-    y = [-item.get(t, 0) for t in Y_ORDER]
+    output_ = {**item}
+    y = [-item.get(t, 0) for t in OIL_KINDS]
     d = (
         (-x[2]) * x[4] * x[6]
         + x[1] * x[5] * x[6]
@@ -334,17 +325,22 @@ def oil_solve(item: dict, scheme=None, beautify_output=True):
             for k, v in t.items():
                 v = v * x[i]
                 item[k] = item.get(k, 0) + v
-                if v < 0:
-                    output += fraction_format(-v) + " " + k + ", "
+                temp = result_dict.get(k, 0) + v
+                if temp == 0:
+                    del result_dict[k]
                 else:
-                    input_ += fraction_format(v) + " " + k + ", "
-            record(x[i], X_ORDER[i], output, input_)
-    for k in Y_ORDER:
-        del item[k]
+                    result_dict[k] = temp
+                if v < 0:
+                    output += fraction_format(-v) + " " + translater[k] + ", "
+                else:
+                    input_ += fraction_format(v) + " " + translater[k] + ", "
+            record(x[i], translater[OIL_RECIPES[i]], output, input_)
+    for k in OIL_KINDS:
+        assert item.pop(k, 0) == 0
     if beautify_output:
-        print("Input:")
-        estimate_belts({k: v for k, v in item.items() if k in FLUIDS})
-        print("Output:")
+        print(translater["Input:"])
+        estimate_belts({k: v for k, v in item.items()})
+        print(translater["Output:"])
         estimate_belts(output_)
         print(SEPARATOR)
     return item
@@ -364,24 +360,24 @@ def reduce(advanced, basic, scheme=None, beautify_output=True):
             a = item_dict.pop(i)
             input_item, time, category, name = single_product_dict[i]
             speed, prod = scheme[category]
-            output = fraction_format(a) + " " + i
+            output = fraction_format(a) + " " + translater[i]
             time /= speed
             a = a / prod
             count = a * time
             input_ = ""
             for ii, aa in input_item:
                 aa = aa * a
-                input_ += fraction_format(aa) + " " + ii + ", "
+                input_ += fraction_format(aa) + " " + translater[ii] + ", "
                 if ii in item_dict:
                     item_dict[ii] += aa
                 else:
                     item_dict[ii] = aa
                     heappush(item_heap, (complexity_dict[ii], ii))
-            record(count, category, output, input_)
+            record(count, translater[category], output, input_)
     if beautify_output:
-        print("Input:")
+        print(translater["Input:"])
         estimate_belts(item_dict)
-        print("Output:")
+        print(translater["Output:"])
         estimate_belts(advanced)
         print(SEPARATOR)
     return item_dict
@@ -390,31 +386,57 @@ def reduce(advanced, basic, scheme=None, beautify_output=True):
 def quantitative_factory():
     global result_dict
     for block in factory:
-        if block[0] == "result":
-            print(block[1] + ":")
-            print()
+        if block[1] == "show":
+            print(block[0] + ":\n")
             estimate_belts(result_dict)
             print(SEPARATOR)
-        elif block[0] == "oil-block":
-            print(block[0].upper())
-            print()
-            oil_solve(result_dict, block[1], beautify_output=True)
-        elif block[0] == "science-block":
-            print(block[0].upper())
-            print()
-            output = {**result_dict}
-            result_dict = {}
-            reduce(output, science_packs, block[1], beautify_output=False)
+        elif block[1] == "solve_oil":
+            print(block[0].upper() + "\n")
+            output = {k: result_dict[k] for k in OIL_KINDS}
+            oil_solve(output, block[2], beautify_output=True)
+        elif block[1] == "science":
+            print(block[0].upper() + "\n")
+            output = {k: result_dict.pop(k) for k in ["7sp"]}
+            reduce(output, science_packs, block[2], beautify_output=False)
             print(SEPARATOR)
         else:
             block_name, output, input_, scheme, color = block
             output = {k: result_dict.pop(k) for k in output}
             set_color(color)
-            print(block_name.upper())
-            print()
+            print(block_name.upper() + "\n")
             t = reduce(output, input_, scheme, beautify_output=True)
             set_color("white")
         print()
+
+
+class Translater:
+    def __init__(self, locale: str):
+        self.locale = locale
+        self.locale_dict = LOCALE_DICT.get(locale, {})
+        if locale == "en":
+            return
+        cf = ConfigParser()
+        locale_file_path = FACTORIO_PATH + "data/base/locale/" + locale + "/base.cfg"
+        if not os.path.exists(locale_file_path):
+            raise FileNotFoundError("cannot find locale file in:\n" + locale_file_path)
+        cf.read(locale_file_path, encoding="utf8")
+        self.locale_dict.update(cf["entity-name"])
+        self.locale_dict.update(cf["equipment-name"])
+        self.locale_dict.update(cf["fluid-name"])
+        self.locale_dict.update(cf["item-name"])
+        self.locale_dict.update(cf["recipe-name"])
+
+    def __getitem__(self, word: str):
+        return self.locale_dict[word] if word in self.locale_dict else word
+
+    def get_term_list(self, out_path="terms.txt"):
+        if self.locale == "en":
+            return
+        with open(out_path, "w") as f:
+            for k, v in self.locale_dict.items():
+                if k not in {"script_help"}:
+                    f.write("{0} : {1}\n".format(v, k))
+        os.startfile(out_path)
 
 
 def spm(i):
@@ -435,132 +457,12 @@ def show(d=None):
 
 
 if __name__ == "__main__":
+    config = yaml.full_load(open("config.yaml", encoding="utf-8"))
+    preprocessing_schemes(config)
+    globals().update(config)
     recipe_dict = get_recipe()
     single_product_dict = get_single_product_dict()
     complexity_dict = get_complexity_dict()
+    translater = Translater(LOCALE)
     result_dict = {}
-    normal = {
-        "crafting": (Fraction("1.25"), Fraction(1)),
-        "chemistry": (Fraction(1), Fraction(1)),
-        "smelting": (Fraction(2), Fraction(1)),
-        "oil-processing": (Fraction(1), Fraction(1)),
-        "centrifuging": (Fraction(1), Fraction(1)),
-        "rocket-building": (Fraction(1), Fraction(1)),
-        "lab": (Fraction(1), Fraction(1)),
-        "other": (Fraction(1), Fraction(1)),
-    }
-    modularize = {
-        "crafting": (Fraction("5.5"), Fraction("1.4")),
-        "chemistry": (Fraction("4.55"), Fraction("1.3")),
-        "smelting": (Fraction("9.4"), Fraction("1.2")),
-        "oil-processing": (Fraction("5.55"), Fraction("1.3")),
-        "centrifuging": (Fraction(1), Fraction(1)),
-        "rocket-building": (Fraction("2"), Fraction("1.4")),
-        "lab": (Fraction("10"), Fraction("1.2")),
-        "other": (Fraction(1), Fraction(1)),
-    }
-    raw_items = {
-        "copper-ore",
-        "iron-ore",
-        "stone",
-        "coal",
-        "water",
-        "petroleum-gas",
-        "light-oil",
-        "heavy-oil",
-    }
-    smelted_items = {
-        "copper-plate",
-        "iron-plate",
-        "steel-plate",
-        "stone",
-        "stone-brick",
-        "coal",
-    }
-    chemistry_items = {"sulfuric-acid", "sulfur", "plastic-bar", "lubricant"}
-    bus_items = {
-        "copper-plate",
-        "iron-plate",
-        "steel-plate",
-        "stone",
-        "stone-brick",
-        "coal",
-        "water",
-        "sulfuric-acid",
-        "sulfur",
-        "plastic-bar",
-        "petroleum-gas",
-        "light-oil",
-        "lubricant",
-        "electronic-circuit",
-        "advanced-circuit",
-        "processing-unit",
-    }
-    science_packs = {
-        "automation-science-pack",
-        "logistic-science-pack",
-        "chemical-science-pack",
-        "production-science-pack",
-        "military-science-pack",
-        "utility-science-pack",
-        "space-science-pack",
-    }
-    factory = [
-        ["science-block", normal],
-        ["automation sp block", {"automation-science-pack"}, bus_items, normal, "red"],
-        ["logistic sp block", {"logistic-science-pack"}, bus_items, normal, "green"],
-        ["chemical sp block", {"chemical-science-pack"}, bus_items, normal, "blue"],
-        ["production sp block", {"production-science-pack"}, bus_items, normal, "pink"],
-        ["military sp block", {"military-science-pack"}, bus_items, normal, "gray"],
-        ["utility sp block", {"utility-science-pack"}, bus_items, normal, "yellow"],
-        ["space sp block", {"space-science-pack"}, bus_items, normal, "white"],
-        ["result", "main bus"],
-        [
-            "processing-unit block",
-            {"processing-unit"},
-            smelted_items ^ chemistry_items,
-            normal,
-            "blue",
-        ],
-        [
-            "advanced-circuit block",
-            {"advanced-circuit"},
-            smelted_items ^ chemistry_items,
-            normal,
-            "red",
-        ],
-        [
-            "electronic-circuit block",
-            {"electronic-circuit"},
-            smelted_items ^ chemistry_items,
-            normal,
-            "green",
-        ],
-        ["result", "smelted or chemistry items"],
-        [
-            "chemistry block",
-            chemistry_items,
-            raw_items ^ {"iron-plate"},
-            normal,
-            "white",
-        ],
-        ["smelting block", smelted_items, raw_items, normal, "white"],
-        ["oil-block", normal],
-        ["result", "raw items"],
-    ]
-    code.interact(
-        banner="""
-    This is a simple Read-Eval-Print-Loop
-
-    Examples:
-    spm(60)
-    show()
-    clear()
-    reduce({'utility-science-pack':2}, bus_items, modularize)
-    oil_solve({'petroleum-gas':130, 'light-oil':90, 'heavy-oil':10}, normal)
-
-    type exit() to quit
-    """,
-        local=locals(),
-    )
-
+    code.interact(banner=translater["script_help"], local=locals())
